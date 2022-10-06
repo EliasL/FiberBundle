e2dc0991ef8779bc8dbb4767bd83e71745d1c225
Elias Lundheim <elias.lundheim@gmail.com>
2022-10-06 10:59:03 +0200

    better plot

--- plottingScripts/neighbourhood rules over regiemes.jl
+++ plottingScripts/neighbourhood rules over regiemes.jl
@@ -42,8 +42,8 @@ max_number_of_clusters = [[1/(maximum(files[t_][i]["average_nr_clusters"])*N) fo
 k_where_nr_clusters_is_max = [[argmax(files[t_][i]["average_nr_clusters"]) / N for t_ in t] for i in 1:length(distributions(1))]
 sigma_c = [[maximum(files[t_][i]["average_most_stressed_fiber"]) for t_ in t][1:4] for i in 1:length(distributions(1))]
 k_where_sigma_c_is_max = [[argmax(files[t_][i]["average_most_stressed_fiber"]) / N for t_ in t][1:4] for i in 1:length(distributions(1))]
-largest_cluster_size = [[files[t_][i]["average_largest_cluster"][floor(Int64, N/3)] for t_ in t] for i in 1:length(distributions(1))]
-largest_perimeiter = [[maximum(files[t_][i]["average_largest_perimiter"]) for t_ in t] for i in 1:length(distributions(1))]
+largest_cluster_size = [[files[t_][i]["average_largest_cluster"][floor(Int64, N/3)] for t_ in t][1:4] for i in 1:length(distributions(1))]
+largest_perimeiter = [[files[t_][i]["average_largest_perimiter"][floor(Int64, N/3)] for t_ in t] for i in 1:length(distributions(1))]
 
 max_number_of_clusters_plot = plot(t ./ 10, max_number_of_clusters , label = lables, legend=:topleft,
                     xlabel=L"t_0", ylabel=L"1/N^{\mathrm{max}}_c", title="Maximum of number of clusters")
@@ -57,11 +57,11 @@ sigma_c_plot = plot(t[1:4]  ./ 10, sigma_c, label = lables, legend=:topleft,
 k_where_sigma_c_is_max_plot = plot(t[1:4]  ./ 10, k_where_sigma_c_is_max, label = lables,
                     xlabel=L"t_0", ylabel=L"k/N"*" at "*L"σ_c", title=L"k/N"*" where maximum was reached")
 
-lagrest_clusetr_size_plot = plot(t  ./ 10, largest_cluster_size, label = lables,
+lagrest_clusetr_size_plot = plot(t[1:4]  ./ 10, largest_cluster_size, label = lables, legend=:bottomright,
                     xlabel=L"t_0", ylabel=L"S_{\mathrm{max}}/N", title="Largest cluster size at "*L"k/N=\frac{1}{3}")
 
-largest_perimeiter_plot = plot(t  ./ 10, largest_perimeiter, label = lables,
-                    xlabel=L"t_0", ylabel=L"H_{\mathrm{max}}/N", title="Largest perimiter size")
+largest_perimeiter_plot = plot(t  ./ 10, largest_perimeiter, label = lables, legend=:right,
+                    xlabel=L"t_0", ylabel=L"H_{\mathrm{max}}/N", title="Largest perimiter size at "*L"k/N=\frac{1}{3}")
 
 
 l = @layout [
