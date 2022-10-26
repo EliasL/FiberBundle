622e8980bef3398ec5955e25a1444c5fed729ae4
Elias Lundheim <elias.lundheim@gmail.com>
2022-10-26 09:31:05 +0200

    Alpha run :D

--- job.sh
+++ job.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#SBATCH -J coreTest
+#SBATCH -J alphaItteration
 #SBATCH -p porelab
 #SBATCH -N 1
 #SBATCH -n 64
 #SBATCH --exclusive=user
-#SBATCH --time=0-05:00:00
+#SBATCH --time=0-15:00:00
 
 ml eb
 #ml av
 ml Julia/1.7.2-linux-x86_64
 #julia --threads 1 support/timeEstimator.jl
-julia --threads 50 generateData.jl
+julia --threads 40 generateData.jl
 
 #mpiexecjl -n 4 julia --threads 30 generateData.jl
 
