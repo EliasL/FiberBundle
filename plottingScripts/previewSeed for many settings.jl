8b11bf13f226c7bc4eb56d735fb2002a5c0dfd0c
Elias Lundheim <elias.lundheim@gmail.com>
2022-10-27 11:24:56 +0200

    Nice plots for alpha

--- plottingScripts/previewSeed for many settings.jl
+++ plottingScripts/previewSeed for many settings.jl
@@ -10,12 +10,11 @@ include("../support/dataManager.jl")
 
 function drawmatrix(A::Matrix, color_stress=true, pixel_size = 1)
     L = size(A,1)
-    A = shift_matrix(A)
     tiles = Tiler(L*pixel_size, L*pixel_size, L, L, margin=0)
     cur_colors = palette(:glasbey_category10_n256)
     nr_colors = 256
     if color_stress
-        A = floor.(Int, A./ maximum(A) .* (nr_colors-1)) .+ 1
+        A = ceil.(Int, A./ maximum(A) .* (nr_colors-1))
     end
     stress_colors = cgrad(:heat, nr_colors, categorical=true)
     for (pos, n) in tiles
@@ -23,7 +22,7 @@ function drawmatrix(A::Matrix, color_stress=true, pixel_size = 1)
         fiber = A[tiles.currentcol, tiles.currentrow]
         if color_stress
             box(pos, tiles.tilewidth, tiles.tileheight, :clip)
-            if fiber == 1
+            if fiber == 0
                 star(pos, tiles.tilewidth*0.5, 4, 0.2, pi/4, action=:clip)
                 background(0.2,0.2,0.2)
             else
@@ -49,7 +48,7 @@ function drawmatrix(A::Matrix, color_stress=true, pixel_size = 1)
 end
 
 
-function shift_matrix(m)
+function get_ideal_shift(m)
     # Shift_matrix tries to shift the matrix so that a cluster does#
     # not cross the periodic boarder. In other words, as few broken fibers along
     # the boarder as possible
@@ -62,11 +61,9 @@ function shift_matrix(m)
 
     # Now we check which row and which column has the fewest broken fibers
     # row and col might be swapped here. Haven't chekced.
-    min_row = argmin([sum(view(part_of_largest_cluster, :, i)) for i in 1:L])
-    min_col = argmin([sum(view(part_of_largest_cluster, i, :)) for i in 1:L])
-    println(min_row)
-    println(min_col)
-    return circshift(m, (min_row, min_col))
+    min_row = argmin([sum(view(part_of_largest_cluster, i, :)) for i in 1:L])
+    min_col = argmin([sum(view(part_of_largest_cluster, :, i)) for i in 1:L])
+    return (L-min_row, L-min_col)
 end
 
 
@@ -81,7 +78,7 @@ function load_file(α, NR, settings)
     return load(get_file_name(setting))
 end
 
-function draw_seeds(L, color_stress)
+function draw_seeds(L)
 
     path = "data/"
     dist = "Uniform"
@@ -100,37 +97,53 @@ function draw_seeds(L, color_stress)
     NRS = ["UNR", "SNR", "CNR"]
     α_settings = [1.0, 1.5, 2.0, 3.0, 5.0, 9.0, 15.0]
     lx, ly = (length(NRS),length(α_settings))
-    key = color_stress ? "spanning_cluster_tension" : "spanning_cluster_state"
+    # This will store the best shift of the matrix to center the largest cluster
+    ideal_shifts = []
 
-    grids = reshape([reshape(load_file(α, NR, settings)["$key/$seed"], (L, L)) for NR=NRS, α=α_settings], (lx,ly))
-    grid_names = reshape([latexstring("$NR, \$a=$α\$") for NR=NRS, α=α_settings], (lx,ly))
-    pixel_L = L*ps
-    title_space = ceil(Int, pixel_L/2)
-    subtitle_space = ceil(Int, pixel_L/5)
-    spacing_x = ceil(Int, pixel_L/20)
-    spacing_y = spacing_x + subtitle_space
-    image_size_x = pixel_L*lx+(lx-1)*spacing_x
-    image_size_y = pixel_L*ly+(ly)*spacing_y + title_space
-    font_size = 4*pixel_L/32
-    title_font_size = font_size * 4/3
 
-    stress =  color_stress ? " stress" : ""
-    Drawing(image_size_x, image_size_y, "plots/Visualizations/Spanning/Spanning clusters L=$L$stress.png")
-    background(1,1,1) #White background
-    fontface("Computer Modern")
-    fontsize(title_font_size)
-    Luxor.text(latexstring("Spanning clusters, \$ L=$L\$"), image_size_x/2, title_space*2/3, halign=:center, valign=:bottom)
-    fontsize(font_size)
-    for j=1:ly, i=1:lx
-        origin((pixel_L+spacing_x)*(i-1), (pixel_L+spacing_y)*(j-1) + title_space)
-        drawmatrix(grids[i,j], color_stress, ps)
-        Luxor.text(grid_names[i, j], pixel_L/2, pixel_L+subtitle_space, halign=:center, valign=:bottom)
+    # we first draw the cluster image since we need those to calculate the ideal shifts
+    for key in ["spanning_cluster_state", "spanning_cluster_tension"]
+        grids = reshape([reshape(load_file(α, NR, settings)["$key/$seed"], (L, L)) for NR=NRS, α=α_settings], (lx,ly))
+        grid_names = reshape([latexstring("$NR, \$a=$α\$") for NR=NRS, α=α_settings], (lx,ly))
+        pixel_L = L*ps
+        title_space = ceil(Int, pixel_L/2)
+        subtitle_space = ceil(Int, pixel_L/5)
+        spacing_x = ceil(Int, pixel_L/20)
+        spacing_y = spacing_x + subtitle_space
+        image_size_x = pixel_L*lx+(lx-1)*spacing_x
+        image_size_y = pixel_L*ly+(ly)*spacing_y + title_space
+        font_size = 4*pixel_L/32
+        title_font_size = font_size * 4/3
+
+        
+        stress =  key=="spanning_cluster_tension" 
+        Drawing(image_size_x, image_size_y, "plots/Visualizations/Spanning/Spanning clusters L=$L"*"$(stress ? " stress" : "").png")
+        background(1,1,1) #White background
+        fontface("Computer Modern")
+        fontsize(title_font_size)
+        Luxor.text(latexstring("Spanning clusters, \$ L=$L\$"), image_size_x/2, title_space*2/3, halign=:center, valign=:bottom)
+        fontsize(font_size)
+        for j=1:ly, i=1:lx
+            origin((pixel_L+spacing_x)*(i-1), (pixel_L+spacing_y)*(j-1) + title_space)
+
+            # Get the shift of the matrix and store it so we can use it when drawing stress
+            m = grids[i,j]
+            if key=="spanning_cluster_state"
+                shift = get_ideal_shift(m)
+                push!(ideal_shifts, shift)
+            else
+                shift = popfirst!(ideal_shifts)
+            end
+
+            drawmatrix(circshift(m, shift), stress, ps)
+            Luxor.text(grid_names[i, j], pixel_L/2, pixel_L+subtitle_space, halign=:center, valign=:bottom)
+        end
+
+        finish()
     end
 
-    finish()
+
 end
 
-draw_seeds(32, true)
-draw_seeds(32, false)
-draw_seeds(64, true)
-draw_seeds(64, false)
+draw_seeds(32)
+draw_seeds(64)
