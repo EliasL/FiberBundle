6d56bf58ba57f2bb795f3d5679ece90e1fa9ba05
Elias Lundheim <elias.lundheim@gmail.com>
2022-11-24 15:21:21 +0100

    this is the opposite of what i want...

--- plottingScripts/dimension_of_clusters.jl
+++ plottingScripts/dimension_of_clusters.jl
@@ -30,45 +30,56 @@ function plot_dimension_thing()
     
     L = [8,16,32,64,128,256,512,1024]
     t = [0.0]
-    nr = ["SNR", "UNR"]
+    NR = ["SNR", "UNR"]
     N = L.*L
     α = 2.0
 
-    files = [load_file(l, α, t[1], nr[2]) for l in L]
-    #seeds = last(files)["nr_seeds_used"]
-    #println(seeds)
-    s = [f["average_spanning_cluster_size"] for f in files]
-    h = [f["average_spanning_cluster_perimiter"] for f in files]
+    function get_s_and_h_plots(nr)
+        files = [load_file(l, α, t[1], nr) for l in L]
+        #seeds = last(files)["nr_seeds_used"]
+        #println(seeds)
+        s = [f["average_spanning_cluster_size"] for f in files]
+        h = [f["average_spanning_cluster_perimiter"] for f in files]
 
-    std_s = [f["std_spanning_cluster_size"] for f in files]
-    std_h = [f["std_spanning_cluster_perimiter"] for f in files]
+        std_s = [f["std_spanning_cluster_size"] for f in files]
+        std_h = [f["std_spanning_cluster_perimiter"] for f in files]
 
-    L = log2.(L)
-    s = log2.(s.± std_s)
-    h = log2.(h.± std_s)
+        log_L = log2.(L)
+        s = log2.(s.± std_s)
+        h = log2.(h.± std_s)
 
-    fit_s = get_fit(L, Measurements.value.(s))
-    fit_h = get_fit(L, Measurements.value.(h))
-    slope_s = round(fit_s[2], digits=2)
-    slope_h = round(fit_h[2], digits=2)
+        fit_s = get_fit(log_L, Measurements.value.(s))
+        fit_h = get_fit(log_L, Measurements.value.(h))
+        slope_s = round(fit_s[2], digits=2)
+        slope_h = round(fit_h[2], digits=2)
 
+        s = s .± log2.(std_s)
+        h = h .± log2.(std_h)
 
-    s = s .± log2.(std_s)
-    h = h .± log2.(std_h)
-    m_shape = :cross
-    default(color=:black, markersize=3)
-    s_plot = scatter(L, s, markershape=m_shape, label=L"Cluster size ($s$)", legend=:topleft, xlabel=L"log$_2(L)$", ylabel=L"log$_2(s)$")
-    plot!(L, f_lin(L, fit_s), label=L"Fit ($D_s$)", color=:black)
-    h_plot = scatter(L, h, markershape=m_shape, label=L"Perimiter length ($h$)", legend=:topleft, xlabel=L"log$_2(L)$", ylabel=L"log$_2(h)$")
-    plot!(L, f_lin(L, fit_h), label=L"Fit ($D_h$)")
-    yticks!(h_plot, yticks(s_plot)[1])
-
+        m_shape = :cross
+        default(color=:black, markersize=3)
+        s_plot = scatter(log_L, s, markershape=m_shape, label=L"Cluster size ($s$)",
+                        legend=:topleft, xlabel=L"log$_2(L)$", ylabel=L"log$_2(s)$",
+                        title=latexstring("$nr: \$D_s=$slope_s\$"))
+        plot!(log_L, f_lin(log_L, fit_s), label=L"Fit ($D_s$)", color=:black)
+        h_plot = scatter(log_L, h, markershape=m_shape, label=L"Perimiter length ($h$)",
+                        legend=:topleft, xlabel=L"log$_2(L)$", ylabel=L"log$_2(h)$",
+                        title=latexstring("$nr: \$D_h=$slope_h\$"))
+        plot!(log_L, f_lin(log_L, fit_h), label=L"Fit ($D_h$)")
+        yticks!(h_plot, yticks(s_plot)[1])
+        return s_plot, h_plot
+    end
+    @assert NR[1] == "SNR"
+    SNR_s_plot, SNR_h_plot = get_s_and_h_plots(NR[1])
+    @assert NR[2] == "UNR"
+    UNR_s_plot, UNR_h_plot = get_s_and_h_plots(NR[2])
 
     l = @layout [
-        A B
+        A B;
+        C D
     ]
-    plot(s_plot, h_plot, size=(500, 300), layout = l,
-    plot_title=latexstring("Dimensionality: \$D_s=$slope_s, D_h=$slope_h\$"))
+    plot(SNR_s_plot, SNR_h_plot, UNR_s_plot, UNR_h_plot, size=(800, 600), layout = l,
+plot_title=latexstring("Dimensionality"))#: \$t=$(t[1])\$"))
 
     savefig("plots/Graphs/dimension.pdf")
 end
