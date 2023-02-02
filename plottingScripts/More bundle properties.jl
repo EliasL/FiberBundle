d0a518facd1e75290bd1fcfd3848e479e3cadcd7
Elias Lundheim <elias.lundheim@gmail.com>
2023-02-02 15:49:06 +0100

    Super Picture! What IS THIS!

--- plottingScripts/More bundle properties.jl
+++ plottingScripts/More bundle properties.jl
@@ -33,38 +33,65 @@ function otherPropertiesPlot(L, ts, NR; use_y_lable=true, add_ELS=true)
     
     yLabel(string) = use_y_lable ? string : ""
     function make_plot(y, ylabel, labels; x=k_N, title="", ylims=(-Inf, Inf), xlabel="", xlims=(-Inf, Inf),
-        position=:topright, series_annotation=[], serries_position=:left)
-
-        p = scatter(x, y, label = labels, legend=position, xlims=xlims, ylims=ylims,  markersize=5, 
+        position=:topright, series_annotation=[], series_position=:left, log=:identity, anotate_both=true)
+        if !anotate_both
+            series_annotation = [["" for _ in ts] text.([t in series_annotation ?  L" t_0 = "*"$t" : "" for t in ts], halign=series_position, valign=:bottom)]
+        else
+            series_annotation = text.([t in series_annotation ?  L" t_0 = "*"$t" : "" for t in ts], halign=series_position, valign=:bottom)
+        end
+        p = scatter(x, y, label = labels, legend=position, xlims=xlims, ylims=ylims,  markersize=5, xaxis=log, yaxis=log,
         xlabel=xlabel, ylabel=yLabel(ylabel), title=title, markershape=[:x :+], mc=:auto, msc=:auto,
-        series_annotations =[ ["" for _ in ts] text.([t in series_annotation ?  L" t_0 = "*"$t" : "" for t in ts], halign=serries_position, valign=:bottom)])
+        series_annotations =series_annotation)
         return p
     end
 
     function make_plot2(y, ylabel, labels; x=k_N, title="", ylims=(-Inf, Inf), xlabel="", xlims=(-Inf, Inf),
-        position=:topright)
+        position=:topright, log=:identity)
 
         p = plot(x, y, label = labels, legend=false, xlims=xlims, ylims=ylims,  markersize=5, 
-        xlabel=xlabel, ylabel=yLabel(ylabel), title=title)
+        xlabel=xlabel, ylabel=yLabel(ylabel), title=title, xaxis=log, yaxis=log)
         return p
     end
 
     labels = permutedims(NR)
 
     nr_clusters = get_data("average_nr_clusters")
+    largest_cluster = get_data("average_largest_cluster")
     largest_cluster_spanning = get_spanning_data("average_largest_cluster")
     largest_perimiter_spanning = get_spanning_data("average_largest_perimiter")
+    most_stressed_fiber_spanning = get_spanning_data("average_most_stressed_fiber")
 
-    perimiter_over_cluster = make_plot(largest_cluster_spanning, L"Spanning $s_{\mathrm{max}}/N$", labels,
-                x=largest_perimiter_spanning, xlabel=L"Spanning $h_{\mathrm{max}}/N$", series_annotation=[0.0, 0.1, 0.3, 0.8])
+    perimiter_over_cluster = make_plot(largest_cluster_spanning, L"Log of spanning $s_{\mathrm{max}}/N$", labels, log=:log, anotate_both=false,
+                x=largest_perimiter_spanning, xlabel=L"Log of spanning $h_{\mathrm{max}}/N$", series_annotation=[0.0, 0.2, 0.3, 0.8])
     max_σ = make_plot([maximum.(most_stressed_fiber_nr) for most_stressed_fiber_nr in most_stressed_fiber],
                         position=:topleft, L"σ_{\mathrm{max}}", labels, x=ts, xlabel=L"t_0")
-    σ_area = make_plot([trapz.([k_N for _ in ts], most_stressed_fiber_nr) for most_stressed_fiber_nr in most_stressed_fiber],
+#=     σ_area = make_plot([trapz.([k_N for _ in ts], most_stressed_fiber_nr) for most_stressed_fiber_nr in most_stressed_fiber],
                         position=:topleft, L"Area under $σ$", labels, x=ts, xlabel=L"t_0")
     # Scale with localization and strenght at that point
     σ_scaled = make_plot2([most_stressed_fiber_nr for most_stressed_fiber_nr in most_stressed_fiber[2]./get_energy_data("average_most_stressed_fiber", divide=1)[2]],
-                        position=:topleft, L"Scaled $σ$", permutedims(ts2), x=[k_N for _ in ts] ./ 1#= energy_x[2] =#, xlabel=L"t_0")
-    other_plots = [perimiter_over_cluster, max_σ, σ_area, σ_scaled]
+                        L"Scaled $σ$", permutedims(ts2), x=[k_N for _ in ts] ./ 1#= energy_x[2] =#, xlabel=L"t_0", position=:topleft,)
+ =#
+    size_over_σ = make_plot(largest_cluster_spanning, log=:log, series_annotation=[0.2, 0.3], series_position=:right,
+                        L"log$(s_{\mathrm{max}}/N)$", labels, x=most_stressed_fiber_spanning, xlabel=L"log$(σ)$", position=:topright, )
+    
+
+    size_over_σ_LLS = make_plot(largest_cluster_spanning[1], log=:log, series_annotation=[0.1, 0.2, 0.3], 
+                        L"log$(s_{\mathrm{max}}/N)$", "LLS", x=most_stressed_fiber_spanning[1], xlabel=L"log$(σ)$", position=:topright, )
+    
+
+    size_over_σ_CLS = make_plot(largest_cluster_spanning[2], log=:log, series_annotation=[0.2, 0.3, 0.4],
+                        L"log$(s_{\mathrm{max}}/N)$", "CLS", x=most_stressed_fiber_spanning[2], xlabel=L"log$(σ)$", position=:topright, )
+    
+             
+    #= σ_over_size = make_plot2(most_stressed_fiber[1], L"LLS $σ$", labels,
+                        x=largest_cluster[1], xlabel=L"LLS $s_{\mathrm{max}}/N$")
+
+    σ_over_size2 = make_plot2(most_stressed_fiber[2], L"CLS $σ$", labels,
+                        x=largest_cluster[2], xlabel=L" CLS $s_{\mathrm{max}}/N$")=#
+
+    
+    
+    other_plots = [size_over_σ_LLS, size_over_σ_CLS]
     return other_plots
 end
 
@@ -73,9 +100,10 @@ L = 128
 nr = ["LLS", "CLS"]
 ts = vcat((0:20) ./ 50, (5:9) ./ 10)
 ts2 = (0:10) ./ 50
+ts2 = vcat((0:20) ./ 50, (5:9) ./ 10)
 #ts = [0.1,0.2]
 plots = otherPropertiesPlot(L, ts, nr)
-p = plot(plots..., size=(700,700))
+p = plot(plots..., size=(700,300))
 savefig(p, "plots/Graphs/otherBundleProperties.svg")
 
 println("Saved plot!")
