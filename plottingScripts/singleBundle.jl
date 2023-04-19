bfbb7bdd598d2a0386a7d59e48cccbd8b96b4bb2
Elias Lundheim <elias.lundheim@gmail.com>
2023-04-19 12:02:38 +0200

    better plot

--- plottingScripts/singleBundle.jl
+++ plottingScripts/singleBundle.jl
@@ -4,26 +4,28 @@ include("../burningMan.jl")
 include("../support/dataManager.jl")
 
 function breakBundle(b::FB, s::FBS, real_values=false)
-    x = zeros(Float64, 2*b.N-1)
-    σ = zeros(Float64, 2*b.N-1)
-    for i in 1:b.N-1
+    x = zeros(Float64, 2*b.N+1)
+    σ = zeros(Float64, 2*b.N+1)
+    b.x[argmax(b.x)] = 0.92
+    for i in 1:b.N
         update_tension!(b)
         find_next_fiber!(b)
         update_storage!(b, s)
         f = b.break_sequence[i]
         if real_values #Not quite working
             X = b.x[f]/b.σ[f] 
-            tension = b.tension[f]*(b.N-b.current_step)
+            tension = b.tension[f]*(b.N-b.current_step+1)
             x[2*i] = X
             x[2*i+1] = x[2*i]
             σ[2*i] = tension
             σ[2*i-1] = tension - b.max_σ
         else
             X = b.x[f] 
+            tension = X * (b.N-b.current_step+1)
             x[2*i] = X
             x[2*i+1] = x[2*i]
-            σ[2*i] = b.max_σ
-            σ[2*i-1] = σ[2*i] - b.max_σ/X * (X-x[2*i-1])
+            σ[2*i] = tension
+            σ[2*i+1] = tension-X
         end
         break_fiber!(b)
         
@@ -88,14 +90,14 @@ end
 
 function make_plot(b::FB, s::FBS)
     x, σ = breakBundle(b, s)
-    p1 = plot(x, σ, legend=:topleft, title="Not " * b.nr, label="", c=:black, xlabel="sort of "*L"x", ylabel="slightly " * L"σ")
+    p1 = plot(x, σ, legend=:topleft, title="No Load Sharing ", label="", c=:black, xlabel="sort of "*L"x", ylabel="slightly " * L"σ")
     healBundle!(b)
     x, σ = slowBreak(b, s)
     p2 = plot(x, σ, legend=:topleft, title="Almost " * b.nr, label="", c=:black, xlabel=L"x", ylabel=L"\tilde{σ}")
     healBundle!(b)
     x, σ = slowBreak(b, s, true)
     p3 = plot(x, σ, legend=:topleft, title=b.nr, label="", c=:black, xlabel=L"x", ylabel=L"σ")
-    return p2, p3
+    return p1, p2, p3
 end
 
 
@@ -121,5 +123,5 @@ CLSPlot = make_plot(b,s)
 #f = load_file(L, α, t, nr, dist, data_path=data_path, average=false)
 #println(maximum(f["most_stressed_fiber/$seed"])*L*L)
 
-plot(LLSPlot..., size=(600, 300), layout= @layout([ A B;]))
+plot(ELSPlot..., size=(900, 300), layout= @layout([ A B C;]))
 savefig("plots/Graphs/ForceOfSingleBundle.pdf")
