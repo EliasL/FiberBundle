4b38619a2654e22328ab27cd00e203137632f806
Elias Lundheim <elias.lundheim@gmail.com>
2023-05-11 14:46:26 +0200

    plots

--- plottingScripts/Basic bundle properties.jl
+++ plottingScripts/Basic bundle properties.jl
@@ -43,6 +43,7 @@ function basicPropertiesPlot(L, ts, nr, dist; use_y_lable=true)
     function add_points(y_data)
         # Add spanning point
         x_data = get_data("average_spanning_cluster_step")
+        println(size(y_data))
         y = [y[round(Int64, x*N)] for (x,y) in zip(x_data,y_data)]
         #Draw spanning
         scatter!(x_data, y, color=colors, label=nothing, markershape=:x, markeralpha=1) 
@@ -75,6 +76,27 @@ function basicPropertiesPlot(L, ts, nr, dist; use_y_lable=true)
         scatter!(x_data/N, y, color=colors, label=nothing, markershape=:+) =#
     end
     
+    function make_none_averaged_σ_plot(L, ts, nr, dist)
+            nr_seeds = 3000
+            divisions=200
+            p = plot()
+            colors = theme_palette(:auto)[1:length(ts)]
+            for (t, c) in zip(ts, colors)
+                raw_σ, x = get_data_kN(L, [nr], t, dist, "most_stressed_fiber",
+                average=false, return_kN=true, divide=1, nr_seeds=nr_seeds)
+                every=round(Int64, L^2/divisions)
+                
+                σ = collect([map(maximum, Iterators.partition(raw_σ[1][:, 1, 1, i], every)) for i in 1:nr_seeds])
+                σ = mean(σ, dims=1)
+                x = collect([map(maximum, Iterators.partition(x[1], every)) for i in 1:nr_seeds])
+                
+                p = plot!(x,  σ, label="", title=nr[1]*" "*L"t_0 = "*"$(ts[1])",
+                    xlabel=L"k/N", ylabel="σ", c=c, alpha=0.05)
+                add_points(mean(raw_σ[1][:, 1, 1, :], dims=2)[1])
+            end
+            return p
+    end
+    
     yLabel(string) = use_y_lable ? string : ""
     function make_plot(y, ylabel; x=k_N, title="", ylims=(-Inf, Inf), xlabel="", xlims=(0, 1.3), position=:topright)
         # Use empty scatter as title
@@ -92,7 +114,8 @@ function basicPropertiesPlot(L, ts, nr, dist; use_y_lable=true)
     most_stressed_fiber = get_data("average_most_stressed_fiber", divide=1)
 
 
-    most_stressed_fiber_plot = make_plot(most_stressed_fiber,L"\langle σ \rangle", title=nr*(nr=="LLS" && add_ELS ? " and ELS" : ""))
+#    most_stressed_fiber_plot = make_plot(most_stressed_fiber,L"\langle σ \rangle", title=nr*(nr=="LLS" && add_ELS ? " and ELS" : ""))
+    most_stressed_fiber_plot = make_none_averaged_σ_plot(L, ts, nr, dist)
     largestluster_plot = make_plot(largestluster,L"\langle s_{\mathrm{max}}/N \rangle")    
     largest_perimiter_plot = make_plot(largest_perimiter,L"\langle h_{\mathrm{max}}/N \rangle", ylims=(0,0.375), xlabel=L"k/N")
     nrlusters_plot = make_plot(nrlusters, L"\langle M/N \rangle", ylims=(0,0.13))
@@ -100,20 +123,6 @@ function basicPropertiesPlot(L, ts, nr, dist; use_y_lable=true)
     return basic_plots
 end
 
-
-function make_none_averaged_σ_plot(L, ts, nr, dist)
-        nr_seeds = 200
-        σ, x = get_data_kN(L, nr, ts, dist, "most_stressed_fiber",
-        average=false, return_kN=true, divide=1, nr_seeds=nr_seeds)
-        every=round(Int64, L^2/50)
-        
-        σ = collect([map(mean, Iterators.partition(σ[1][:, 1, 1, i], every)) for i in 1:nr_seeds])
-        x = collect([map(mean, Iterators.partition(x[1], every)) for i in 1:nr_seeds])
-        
-        p = plot(x,  σ, label="", title=nr[1]*" "*L"t_0 = "*"$(ts[1])",
-            xlabel=L"k/N", ylabel="σ", c=:black, alpha=0.05)
-        return p
-end
 
 L = 128
 ts = round.((1 .- vcat((0:20) ./ 50, (5:7) ./ 10)) ./2, digits=2)
@@ -123,16 +132,16 @@ ts = [0.1, 0.27, 0.3, 0.35, 0.40, 0.5]
 nr = ["LLS", "CLS"]
 dist = "ConstantAverageUniform"
 nrs = length(nr)
-#= nr_plots = [basicPropertiesPlot(L, ts, nr[i], dist, use_y_lable=i==1) for i in 1:nrs]
+nr_plots = [basicPropertiesPlot(L, ts, nr[i], dist, use_y_lable=i==1) for i in 1:nrs]
 plots = reduce(vcat, reduce(vcat, collect.(zip(nr_plots...))))
 p = plot(plots..., layout=(length(plots)÷nrs,nrs), size=(700,800), left_margin=2Plots.mm, link=:x)
 
-savefig(p, "plots/Graphs/$(dist)_BundleProperties.svg") =#
-L=512
+savefig(p, "plots/Graphs/$(dist)_BundleProperties.svg")
+#= L=512
 ts = [0.5, 0.4, 0.3]
-p = [make_none_averaged_σ_plot(L, [ts], [nr], dist) for nr=nr, ts=ts]
+p = [make_none_averaged_σ_plot(L, ts, [nr], dist) for nr=nr]
 #= p1 = make_none_averaged_σ_plot(L, [0.4], ["LLS"], dist)
 p2 = make_none_averaged_σ_plot(L, [0.4], ["CLS"], dist) =#
-p = plot(p..., size=(300*length(nr),300*length(ts)), layout=reverse(size(p)))
-savefig(p, "plots/Graphs/non_averaged.pdf")
+p = plot(p..., size=(300*length(nr), 300), layout=(1,length(nr)))
+savefig(p, "plots/Graphs/non_averaged.pdf") =#
 println("Saved plot!")
